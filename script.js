1c481d768bdb1e17e0377972917dfe769ddb1f8a
brookeslight <46109714+brookeslight@users.noreply.github.com>
2020-06-22 17:18:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const canvas = document.querySelector('.Canvas');
-const width = canvas.width;
-const height = canvas.height;
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'rgb(200, 0, 0)';
+ctx.fillRect(0, 0, width, height);
+
 ctx.fillStyle = 'rgb(54, 81, 94)';
 for(var i = 0; i < 10; i++) {
   ctx.fillRect(25+(10*i), 25+(10*i), 10, 10);
