ab605501e4d3f6997ae0f1d8e9bc9d1274f84992
brookeslight <46109714+brookeslight@users.noreply.github.com>
2020-06-22 17:01:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.querySelector('.myCanvas');
+const canvas = document.querySelector('.Canvas');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
