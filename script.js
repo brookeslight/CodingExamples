07bec72f07a6f7e3da09dc5ec168b23b896e2676
brookeslight <46109714+brookeslight@users.noreply.github.com>
2020-06-22 17:13:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('.Canvas');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const width = canvas.width;
+const height = canvas.height;
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'rgb(51, 84, 44)';
 ctx.fillRect(25, 100, 10, 10);
